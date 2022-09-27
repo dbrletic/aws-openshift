73d74d46fe5d6aa085032e259ed0fbf776dba340
Andrew Brletich <dbrletic@redhat.com>
2022-09-27 17:18:13 -0400

    Added in code and prop file

---
+++ src/main/java/API.java
@@ -0,0 +1,41 @@
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+public class API extends RouteBuilder {
+
+  @Produces 
+  @Named("awsSNSClient")
+   public SnsClient creteSnsClient() {
+    return SnsClient.create();
+  }
+
+  @Produces 
+  @Named("awsSQSClient")
+   public SqsClient createSqsClient() {
+    return SqsClient.create();
+  }
+
+
+
+  @Override
+  public void configure() throws Exception {
+    
+    from("direct:start")
+      .routeId("SNS-Poll")
+      .to("aws2-sns://test.fifo?amazonSNSClient=#awsSNSClient");
+
+    from("aws2-sqs://test-fifo?amazonSQSClient=#awsSQSClient&delay=50&maxMessagesPerPoll=5")
+      .routeId("SQS-client")
+      .to("stream:out");
+
+  }
+
+}
